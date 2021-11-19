bb16f86ca63c6df7cffae04a0372fbe3342402d7
Scott Hutcheson <scott@smhutch.co.uk>
2021-11-19 10:03:37 +0000

    improve index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,53 @@
 import Link from "next/link";
 
+interface Item {
+  href: string;
+  link: string;
+  description: string;
+}
+
+const items: Item[] = [
+  {
+    href: "/earth",
+    link: "Earth",
+    description: "basic texture map",
+  },
+  {
+    href: "/torus",
+    link: "Torus",
+    description: "displaced texture map",
+  },
+  {
+    href: "/box",
+    link: "Box",
+    description: "orthographic instanced group",
+  },
+  {
+    href: "/basic-shader",
+    link: "Basic",
+    description: "simple shader gradient",
+  },
+  {
+    href: "/dotted-shader",
+    link: "Dotted",
+    description: "dotted fragment shader",
+  },
+];
+
 const Index = () => {
   return (
     <main>
       <ul>
-        <li>
-          <Link href="/earth">
-            <a>Earth</a>
-          </Link>
-          : A basic texture map
-        </li>
-        <li>
-          <Link href="/torus">
-            <a>Torus</a>
-          </Link>
-          : Complex texture maps
-        </li>
+        {items.map((item) => {
+          return (
+            <li key={item.href}>
+              <Link href={item.href}>
+                <a>{item.link}</a>
+              </Link>{" "}
+              — {item.description}
+            </li>
+          );
+        })}
       </ul>
     </main>
   );
